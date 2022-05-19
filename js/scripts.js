cbe7da4be81b167c0149f9844eae52564a4e9343
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-05-18 23:38:30 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-let favoriteFood = ('Ramen');
-document.write(favoriteFood);
+let pokemonList [
+  {name: 'Bulbasaur' , height: '0.7 m' , type: ['Grass' , 'Poison']},
+  {name: 'Charmander' , height: '0.6 m' , type: 'Fire'},
+  {name: 'Squirtle' , height: '0.5 m' , type: 'Water'},
+  {name: 'Pikachu' , height: '0.4 m' , type: 'Electric'}
+  ];
+  
