afd1f8968469ffe74635e40d9ecac16ab95cec92
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-05-21 01:19:32 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 let pokemonList = [
-  {name: 'Bulbasaur' , height: '0.7 m' , type: ['Grass' , 'Poison']},
-  {name: 'Ivysaur' , height: '1 m', type: ['Grass' , 'Poison']},
-  {name: 'Charmander' , height: '0.6 m' , type: 'Fire'},
-  {name: 'Charmeleon' , height: '1.1 m' , type: 'Fire'},
-  {name: 'Squirtle' , height: '0.5 m' , type: 'Water'},
-  {name: 'Wartortle' , height: '1.1 m' , type: 'Water'}, 
-  {name: 'Pikachu' , height: '0.4 m' , type: 'Electric'},
-  {name: 'Raichu' , height: '0.8 m' , type: 'Electric'}
+  {name: 'Bulbasaur' , height: '0.7' , type: ['Grass' , 'Poison']},
+  {name: 'Ivysaur' , height: '1', type: ['Grass' , 'Poison']},
+  {name: 'Charmander' , height: '0.6' , type: 'Fire'},
+  {name: 'Charmeleon' , height: '1.1' , type: 'Fire'},
+  {name: 'Squirtle' , height: '0.5' , type: 'Water'},
+  {name: 'Wartortle' , height: '1.1' , type: 'Water'}, 
+  {name: 'Pikachu' , height: '0.4' , type: 'Electric'},
+  {name: 'Raichu' , height: '0.8' , type: 'Electric'}
   ];
  
 for (let i=0; i<pokemonList.length; i++){
   document.write(pokemonList[i].name + ' ' + 'height is' + pokemonList[i].height + ' m!');
         //conditional         
-  if(pokemonList[i].height >= 1.0 m){
+  if(pokemonList[i].height >= 1.0){
     document.write ('   - Wow, that\'s big!' + '<br>');
   }
 }
