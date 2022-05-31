af4caa5b5bd76d388e38d19588dc26e5da16c03b
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-05-31 17:15:56 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
+  let repository = [
   {name: 'Bulbasaur' , height: '0.7' , type: ['Grass' , 'Poison']},
   {name: 'Ivysaur' , height: '1', type: ['Grass' , 'Poison']},
   {name: 'Charmander' , height: '0.6' , type: 'Fire'},
@@ -11,15 +11,23 @@ let pokemonRepository = (function () {
   ];
 
   function add(pokemon) {
-   pokemonList.push(pokemon);
+    if {
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon } {
+   repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
+  }
   
   function getAll() {
-    return pokemonList;
+    return repository;
     }
   
   function size(pokemon){
-      document.write(pokemon.name + ' ' + 'height is ' + pokemon.height + ' m! ');
+    document.write(pokemon.name + ' ' + 'height is ' + pokemon.height + ' m! ');
         //conditional         
     if(pokemon.height >= 1.0){
       document.write ('   - Wow, that\'s big!' + '<br>');
@@ -41,5 +49,11 @@ pokemonRepository.add({ name: "Venusaur", height: '2.0', type: ['Grass', 'Poison
 console.log(pokemonRepository.getAll());
   
 pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.size(pokemon)
+  let pokemonList = document.querySelector('pokemon-list');
+  let listpokemon = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = "pokemon.name";
+  button.classList.add('button-class');
+  listpokemon.appendChild(button);
+  pokemonlist.appendChild(listpokemon);
 });
