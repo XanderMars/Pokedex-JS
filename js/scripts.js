262f0e84eec5bd4138e2461620bb98bfa8603bd8
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-06-01 04:18:46 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
+  let repository = [
   {name: 'Bulbasaur' , height: '0.7' , type: ['Grass' , 'Poison']},
   {name: 'Ivysaur' , height: '1', type: ['Grass' , 'Poison']},
   {name: 'Charmander' , height: '0.6' , type: 'Fire'},
@@ -11,28 +11,42 @@ let pokemonRepository = (function () {
   ];
 
   function add(pokemon) {
-   pokemonList.push(pokemon);
+    if {
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon } {
+   repository.push(pokemon);
+    } else {
+      console.log("Invalid Pokemon Entry");
     }
+  }
   
   function getAll() {
-    return pokemonList;
+    return repository;
     }
   
-  function size(pokemon){
-      document.write(pokemon.name + ' ' + 'height is ' + pokemon.height + ' m! ');
-        //conditional         
-    if(pokemon.height >= 1.0){
-      document.write ('   - Wow, that\'s big!' + '<br>');
-    }
-    else {
-        document.write('<br>');
-    }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = "pokemon.name";
+    button.classList.add('button-class');
+    listpokemon.appendChild(button);
+    pokemonlist.appendChild(listpokemon);
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon)
+    });
+  }
+  
+  function showDetails(pokemon) {
+    console.log()
   }
 
   return {
     add: add,
     getAll: getAll,
-    size: size
+    addListItem: addListItem
   };
     
 })();
@@ -41,5 +55,5 @@ pokemonRepository.add({ name: "Venusaur", height: '2.0', type: ['Grass', 'Poison
 console.log(pokemonRepository.getAll());
   
 pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.size(pokemon)
+ pokemonRepository.addListItem(pokemon);
 });
