639ed89142172aeba71a1de2ae5584cb0ff0d00a
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-06-03 04:12:06 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,14 @@ let pokemonRepository = (function () {
     getAll: getAll,
     addListItem: addListItem
   };
+  
+  fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+  return response.json(); // This returns a promise!
+}).then(function (pokemonList) {
+  console.log(pokemonList); // The actual JSON response
+}).catch(function () {
+  // Error
+});
     
 })();
 
