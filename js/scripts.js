8c2f23290b7911c32cd8fab22d08dddf35dc3ca4
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-06-10 15:48:21 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  let modalContainer = document.querySelector('#modal-container');
 
 //Function to add pokemon and validate the typeof
   function add(pokemon) {
